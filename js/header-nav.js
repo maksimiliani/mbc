090270096e37fa8c2e3bc30c0e22330b21fc8868
maksimiliani <macseg@gmail.com>
2021-03-26 12:33:32 +0200

    Update header-nav.js

--- js/header-nav.js
+++ js/header-nav.js
@@ -33,6 +33,20 @@ function update_header(colourr) {
     }
   }
 
+  function convertHex(hexCode,opacity){
+    var hex = hexCode.replace('#','');
+
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+
+    var r = parseInt(hex.substring(0,2), 16),
+        g = parseInt(hex.substring(2,4), 16),
+        b = parseInt(hex.substring(4,6), 16);
+
+    return 'rgba('+r+','+g+','+b+','+opacity/100+')';
+  }
+
   header_el = $(".navbar-master");
   bodyRect = document.body.getBoundingClientRect();
 
@@ -47,9 +61,8 @@ function update_header(colourr) {
           new_colourr = colourr;
         }
 
-        //header_el[0].style.backgroundColor = convertHex(new_colourr, 80);
-        var navbar_bg = $('.navbar-master .navbar-bg');
-        navbar_bg[0].style.backgroundColor = new_colourr;
+        header_el[0].style.backgroundColor = convertHex(new_colourr, 80);
+        console.log(convertHex(new_colourr, 80));
 
         if (isNight(new_colourr)) {
           $('.navbar-master .nav-link').addClass('night');
