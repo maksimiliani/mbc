a68bbef4284020f27b77825e59c03cdf61000f2c
maksimiliani <macseg@gmail.com>
2021-03-31 02:41:37 +0300

    Update header-nav.js

--- js/header-nav.js
+++ js/header-nav.js
@@ -109,7 +109,7 @@ $(document).ready(function() {
     }
   });
 
-  setInterval(() => function() {console.log(locked); locked = false;}, 2000);
+  setInterval(function(){ console.log(locked); locked = false; }, 2000);
   update_header(null);
 
   // $(".w-nav-overlay").attrchange({
