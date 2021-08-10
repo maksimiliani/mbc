e379e29cb811ab59b2bac36af9708ceaf0a693b5
maksimiliani <macseg@gmail.com>
2021-08-10 23:46:46 +0300

    Update header-nav.js

--- js/header-nav.js
+++ js/header-nav.js
@@ -162,7 +162,7 @@ $(document).ready(function() {
       $( this ).css( "color", "red" );
       $('.services-menu')[0].css("height", "calc(100vh - 240px)");// style.height = 'calc(100vh - 240px)';
       $('.services-menu.night')[0].css("scrollbar-color", "rgba(255,255,255,0.33) transparent"); //.style.scrollbar-color = 'rgba(255,255,255,0.33) transparent';
-      $('.services-menu')[0]..css("scrollbar-color", "rgba(42,40,41,0.33) transparent"); //style.scrollbar-color = 'rgba(42,40,41,0.33) transparent';
+      $('.services-menu')[0].css("scrollbar-color", "rgba(42,40,41,0.33) transparent"); //style.scrollbar-color = 'rgba(42,40,41,0.33) transparent';
     }
   });
   $(".megamenu-close").click(function() {
