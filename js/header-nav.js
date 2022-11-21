0d2f3739dc06e89b40ddb8d00975a0bb9294d768
maksimiliani <40344223+maksimiliani@users.noreply.github.com>
2022-11-21 02:18:41 -0700

    ga disabled

--- js/header-nav.js
+++ js/header-nav.js
@@ -122,20 +122,20 @@ $(document).ready(function () {
   if ($('#Page')) {
     $('#Page').val(document.title);
   }
-
-  if ($('#g_id')) {
-    function check_ga() {
-      if (typeof ga === 'function') {
-        ga.getAll().forEach((tracker) => {
-          $('#g_id').val(tracker.get('clientId'));
-        })
-      } else {
-        setTimeout(check_ga, 500);
+  /*
+    if ($('#g_id')) {
+      function check_ga() {
+        if (typeof ga === 'function') {
+          ga.getAll().forEach((tracker) => {
+            $('#g_id').val(tracker.get('clientId'));
+          })
+        } else {
+          setTimeout(check_ga, 500);
+        }
       }
+      check_ga();
     }
-    check_ga();
-  }
-
+  */
   //add all to case page
   tabs = $('#stacked_types');
   if (tabs.length > 0) {
